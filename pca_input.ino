6ba38781ebfa49e3e2ac440554284f3f9ea8670f
Jeong Boin <54930076+boing-86@users.noreply.github.com>
2021-11-07 21:35:56 +0900

    Update pca_input.ino

--- pca_input.ino
+++ pca_input.ino
@@ -1,10 +1,3 @@
-
-#if (ARDUINO >= 100)
- #include <Arduino.h>
-#else
- #include <WProgram.h>
-#endif
-
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 #include <ros.h>
@@ -16,7 +9,8 @@ int servo, throttle, rx, ry;
 
 void pwm_cb(const std_msgs::Int32& cmd_msg){
   servo = cmd_msg.data; //set servo angle, should be from 0-180
-  throttle = 65;
+  Serial.println(servo);
+  throttle = 78;
   rx = constrain(map(servo,0,180,150,600),150,600);
   ry = constrain(map(throttle,0,180,150,600),150,600);
   pwm.setPWM(0,0,rx); //pca9685
@@ -24,29 +18,24 @@ void pwm_cb(const std_msgs::Int32& cmd_msg){
 }
 
 ros::Subscriber<std_msgs::Int32> sub("/poffice_cmd", pwm_cb);
+//ros::Subscriber<morai_msgs::Poffice> pof("/poffic_cmd", pwm_cb);
 
 void setup(){
-  Serial.begin(57600);
+  Serial.begin(115200);
+  throttle = 60;
+  ry = constrain(map(throttle,0,180,150,600),150,600);
+  pwm.setPWM(1,0,ry); //pca9685
+  nh.getHardware()->setBaud(115200);
+  nh.initNode();
   pwm.begin();
   pwm.setPWMFreq(51);
-  nh.initNode();
   nh.subscribe(sub);
-
-  //done setting
-  rx = constrain(map(80,0,180,150,600),150,600);
-  ry = constrain(map(65,0,180,150,600),150,600);
-  pwm.setPWM(0,0,rx); //pca9685
-  pwm.setPWM(1,0,ry);
-  delay(1000);
-  rx = constrain(map(80,0,180,150,600),150,600);
-  ry = constrain(map(0,0,180,150,600),150,600);
-  pwm.setPWM(0,0,rx); //pca9685
+  ry = constrain(map(throttle,0,180,150,600),150,600);
   pwm.setPWM(1,0,ry);
 }
 
+
 void loop(){
-  if(Serial.available()){
     nh.spinOnce();
     delay(1);
-  }
 }
